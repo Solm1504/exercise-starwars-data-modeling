ab89db29a7b622e3a4079bfc094ab3708559027b
Solm1504 <solm1504@gmail.com>
2023-04-16 15:50:14 +0000

    Shineed

--- src/models.py
+++ src/models.py
@@ -7,23 +7,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey ('user.id'))
+    basic_data_id = Column(Integer)
+
+    basic_data = relationship("Basic_data", back_populates="favorites")
+
+class Basic_data(Base):
+    __tablename__ = 'basic_data'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+    favorites = relationship("Favorites", back_populates="basic_data")
+    character = relationship("Character", uselist=False, back_populates="basic_data")
+    planet = relationship("Planet", uselist=False, back_populates="basic_data")
+    
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    gender = Column(String)
+    hair_color = Column(String)
+    eye_color = Column(String)
+    basic_data_id = Column(Integer, ForeignKey('basic_data.id'))
+
+    basic_data = relationship("Basic_data", back_populates="character")
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    population = Column(Integer)
+    terrain = Column(String)
+    climate = Column(String)
+    orbital_period = Column(Integer)
+    rotation_period = Column(Integer)
+    diameter = Column(Integer)
+    basic_data_id = Column(Integer, ForeignKey('basic_data.id'))
+
+    basic_data = relationship("Basic_data", back_populates="planet")
+
+
+
 
     def to_dict(self):
         return {}
